5239858d2a457684976efe9328206e8ea4e30ef1
080812 <74765678+080812@users.noreply.github.com>
2021-11-12 20:34:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,18 +3,21 @@ function preload(){
 }
 
 function setup(){
-    canvas=createCanvas(450,550)
+    canvas=createCanvas(450,460)
     canvas.center()
     video=createCapture(VIDEO)
     video.hide()
 }
 
 function draw(){
-    image(video,0,0,700,770)
+    image(video,90,100,300,220)
 
    fill(40,90,460,90)
    stroke(40,90,460,90)
-   
+   rect(10,10,10,430)
+   rect(430,10,10,430)
+   rect(10,10,430,10)
+   rect(20,430,420,10)
 }
 
 function take_snapshot(){  
